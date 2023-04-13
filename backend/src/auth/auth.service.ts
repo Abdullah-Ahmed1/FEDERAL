dd3cd0f2fb9d3b4309de88fefbcb56d705fa241c
Abdullah Ahmed <abdullah.ahmed10001@gmail.com>
2023-04-13 15:25:11 +0500

    code cleaning

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -4,58 +4,51 @@ import { Request } from 'express';
 import { JwtAuthService } from '../jwt/jwt-auth.service';
 @Injectable()
 export class AuthService {
-    constructor(private prisma : PrismaService,private jwtAuthService: JwtAuthService){}
+    constructor(private prisma: PrismaService, private jwtAuthService: JwtAuthService) { }
 
-    async signup(req : Request){
+    async signup(req: Request) {
         console.log(req.body.phone)
-        try{
+        try {
             const user = await this.prisma.user.create({
-                data:{
-                    username : req.body.username,
-                    email : req.body.email,
-                    password : req.body.password,
-                    phone : req.body.phone 
+                data: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    phone: req.body.phone
                 }
-            }) 
+            })
             console.log(user)
-            return {msg : "this is a signup function"}
-        }catch(err){
+            return { msg: "this is a signup function" }
+        } catch (err) {
             console.log(err)
         }
     }
 
-    signin(req : Request){
+    signin(req: Request) {
         console.log(req.body)
-        return {msg : "this is a signed in function"}
+        return { msg: "this is a signed in function" }
     }
 
-    googleLogin(req,res) {
-        try { 
+    googleLogin(req, res) {
+        try {
             if (!req.user) {
                 return 'No user from google';
-              }
-              const { accessToken } = this.jwtAuthService.login(req.user);
-              res.cookie('jwt', accessToken, {
+            }
+            const { accessToken } = this.jwtAuthService.login(req.user);
+            res.cookie('jwt', accessToken, {
                 httpOnly: true,
                 sameSite: 'lax',
-              });
-              const response = {
+            });
+            const response = {
                 message: 'User information from google',
                 user: req.user,
-              }
-              res.redirect(`http://localhost:5173?response=${encodeURIComponent(JSON.stringify(response))}`)
-      
-            // return res.send({
-            //     message: 'User information from google',
-            //     user: req.user,
-            // })
-            //   return {
-            //     message: 'User information from google',
-            //     user: req.user,
-            //   };    
-        }catch(err){
-            console.log("------>error is   :",err )
+            }
+            res.redirect(`http://localhost:5173?response=${encodeURIComponent(JSON.stringify(response))}`)
+
+
+        } catch (err) {
+            console.log("------>error is   :", err)
         }
-        
-      }
+
+    }
 }
--- backend/src/jwt/jwt-auth.service.ts
+++ backend/src/jwt/jwt-auth.service.ts
@@ -5,18 +5,18 @@ import { JwtPayload } from './jwt-auth.strategy';
 
 @Injectable()
 export class JwtAuthService {
-  constructor(private jwtService: JwtService) {}
+    constructor(private jwtService: JwtService) { }
 
-  login(user) {
-    try{
-        const payload: JwtPayload = { username: user.username, sub: user.id };
-        return {
-        
-          accessToken: this.jwtService.sign(payload,{secret: process.env.JWT_SECRET}),
-        };
-    }catch(err){
-        console.log("error is  : " ,err)
+    login(user) {
+        try {
+            const payload: JwtPayload = { username: user.username, sub: user.id };
+            return {
+
+                accessToken: this.jwtService.sign(payload, { secret: process.env.JWT_SECRET }),
+            };
+        } catch (err) {
+            console.log("error is  : ", err)
+        }
+
     }
-   
-  }
 }
