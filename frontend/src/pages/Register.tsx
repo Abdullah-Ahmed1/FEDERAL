8a7f2f46e1f33b230145222db797a77d956f5178
Abdullah Ahmed <abdullah.ahmed10001@gmail.com>
2023-04-13 12:05:39 +0500

    responsivesnness added on register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -5,26 +5,26 @@ import Typography from '@mui/material/Typography';
 const Register = ()=>{
     return (
         <Grid2 container justifyContent={'center'} >
-           <Grid2 container   flexDirection={'column'} alignItems={'center'} lg ={5} sx  = {{backgroundColor:'white'}}>
+           <Grid2 container   flexDirection={'column'} alignItems={'center'} lg ={5} xs = {11} sx  = {{backgroundColor:'white',marginTop:{xs :'100px'}}}>
                 <Grid2>
                     <h1 style={{fontStyle:"italic"}}>REGISTER</h1>
                 </Grid2>
-                <Grid2 lg = {10} sx = {{marginBottom:"10px"}}>
+                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
                 <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='First Name'/>
                 </Grid2>
-                <Grid2 lg = {10} sx = {{marginBottom:"10px"}}>
+                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
                 <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='Last Name'/>
                 </Grid2>
-                <Grid2 lg = {10} sx = {{marginBottom:"10px"}}>
+                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
                 <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='Email'/>
                 </Grid2>
-                <Grid2 lg = {10} sx = {{marginBottom:"10px"}}>
+                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
                 <TextField id="outlined-basic" fullWidth  variant="outlined" placeholder='Password' />
                 </Grid2>
-                <Grid2 lg = {10} sx = {{marginBottom:"10px"}}>
+                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
                 <TextField id="outlined-basic" fullWidth  variant="outlined" placeholder='Confirm Password' />
                 </Grid2>
-                <Grid2 lg = {10} >
+                <Grid2 lg = {10} xs = {11} >
                 <Button fullWidth sx = {{height:"50px",backgroundColor:"#D8232F"}} variant="contained" color="success" >
                     Submit
                 </Button>
