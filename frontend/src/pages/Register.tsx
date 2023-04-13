815d2b0b47acb09e117faa863c0f10ed1ee5912d
Abdullah Ahmed <abdullah.ahmed10001@gmail.com>
2023-04-13 12:16:56 +0500

    responsiveness added on login page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,40 +2,40 @@ import Grid2 from '@mui/material/Unstable_Grid2'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-const Register = ()=>{
+const Register = () => {
     return (
         <Grid2 container justifyContent={'center'} >
-           <Grid2 container   flexDirection={'column'} alignItems={'center'} lg ={5} xs = {11} sx  = {{backgroundColor:'white',marginTop:{xs :'100px'}}}>
+            <Grid2 container flexDirection={'column'} alignItems={'center'} lg={5} xs={11} sx={{ backgroundColor: 'white', marginTop: { xs: '100px' } }}>
                 <Grid2>
-                    <h1 style={{fontStyle:"italic"}}>REGISTER</h1>
+                    <h1 style={{ fontStyle: "italic" }}>REGISTER</h1>
                 </Grid2>
-                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
-                <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='First Name'/>
+                <Grid2 lg={10} xs={11} sx={{ marginBottom: "10px" }}>
+                    <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='First Name' />
                 </Grid2>
-                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
-                <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='Last Name'/>
+                <Grid2 lg={10} xs={11} sx={{ marginBottom: "10px" }}>
+                    <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='Last Name' />
                 </Grid2>
-                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
-                <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='Email'/>
+                <Grid2 lg={10} xs={11} sx={{ marginBottom: "10px" }}>
+                    <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='Email' />
                 </Grid2>
-                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
-                <TextField id="outlined-basic" fullWidth  variant="outlined" placeholder='Password' />
+                <Grid2 lg={10} xs={11} sx={{ marginBottom: "10px" }}>
+                    <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='Password' />
                 </Grid2>
-                <Grid2 lg = {10} xs = {11} sx = {{marginBottom:"10px"}}>
-                <TextField id="outlined-basic" fullWidth  variant="outlined" placeholder='Confirm Password' />
+                <Grid2 lg={10} xs={11} sx={{ marginBottom: "10px" }}>
+                    <TextField id="outlined-basic" fullWidth variant="outlined" placeholder='Confirm Password' />
                 </Grid2>
-                <Grid2 lg = {10} xs = {11} >
-                <Button fullWidth sx = {{height:"50px",backgroundColor:"#D8232F"}} variant="contained" color="success" >
-                    Submit
-                </Button>
+                <Grid2 lg={10} xs={11} >
+                    <Button fullWidth sx={{ height: "50px", backgroundColor: "#D8232F" }} variant="contained" color="success" >
+                        Submit
+                    </Button>
                 </Grid2>
-                <Grid2 sx = {{padding:"20px 0px 0px 0px"}}>
-                    <Typography sx = {{color:"#D8232F"}} >Forget your Password?</Typography>
+                <Grid2 sx={{ padding: "20px 0px 0px 0px" }}>
+                    <Typography sx={{ color: "#D8232F" }} >Forget your Password?</Typography>
                 </Grid2>
-                <Grid2 sx = {{padding:"20px 0px 20px 0px"}}>
-                    <Typography sx = {{color:"#D8232F"}} >Create An Account</Typography>
+                <Grid2 sx={{ padding: "20px 0px 20px 0px" }}>
+                    <Typography sx={{ color: "#D8232F" }} >Create An Account</Typography>
                 </Grid2>
-           </Grid2>
+            </Grid2>
         </Grid2>
     )
 }
