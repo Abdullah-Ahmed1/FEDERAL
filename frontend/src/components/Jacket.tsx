6254e467e8810ddc8ab7e9df06f19da2b245e555
Abdullah Ahmed <abdullah.ahmed10001@gmail.com>
2023-04-17 14:47:39 +0500

    jackets page added

--- frontend/src/components/Jacket.tsx
+++
@@ -1,9 +0,0 @@
-import Grid2 from '@mui/material/Unstable_Grid2'
-const Jacket = ()=>{
-    return(
-        <>
-            <img style= {{width:"200px"}} src="jacket.jpg" alt="jacket" />
-        </>
-    )
-}
-export default Jacket
--- frontend/src/pages/Jackets.tsx
+++ frontend/src/pages/Jackets.tsx
@@ -1,15 +1,36 @@
 import Grid2 from '@mui/material/Unstable_Grid2'
-import Jacket from '../components/Jacket'
-const Jackets = ()=>{
-    return(
-        <div>
-            <h1>Jackets page</h1>
-        <Grid2 container justifyContent={'center'} alignItems={'center'} >
-            <Grid2 lg = {4} >
-                <Jacket/>
+import Jacket from '../components/products/Jacket'
+import Box from '@mui/material/Box';
+const Jackets = () => {
+    return (
+        <Box sx={{ marginBottom: "20px" }}>
+            <h1 style={{ marginLeft: "120px" }}>Jackets</h1>
+            <Grid2 container justifyContent={'center'} alignItems={'center'} >
+                <Grid2 container sx={{ width: { xs: "100%", lg: "85%" } }} justifyContent={'center'} alignItems={'center'} columnSpacing={.1} rowSpacing={0} >
+                    <Grid2 xs={11}>
+                        <Jacket />
+                    </Grid2 >
+                    <Grid2 xs={11}>
+                        <Jacket />
+                    </Grid2 >
+                    <Grid2 xs={11}>
+                        <Jacket />
+                    </Grid2>    
+                    <Grid2 xs={11}>
+                        <Jacket />
+                    </Grid2>
+                    <Grid2 xs={11}>
+                        <Jacket />
+                    </Grid2>
+                    <Grid2 xs={11}>
+                        <Jacket />
+                    </Grid2>
+                    <Grid2 xs={11}>
+                        <Jacket />
+                    </Grid2>
+                </Grid2>
             </Grid2>
-        </Grid2>
-        </div>
+        </Box>
     )
 }
 export default Jackets
